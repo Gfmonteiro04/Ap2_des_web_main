41ff742fb4d1e0460d8c5257771f41a145bb05fa
Gabriel Monteiro <98609413+Gfmonteiro04@users.noreply.github.com>
2023-11-25 11:22:19 -0300

    Update Codigo.js

--- Codigo.js
+++ Codigo.js
@@ -1,51 +1,107 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const createHTMLElement = (tag, text) => {
-    const element = document.createElement(tag);
-    element.textContent = text;
-    return element;
-  };
+const endpoints = {
+    all: "https://botafogo-atletas.mange.li/all",
+    masculino: "https://botafogo-atletas.mange.li/masculino",
+    feminino: "https://botafogo-atletas.mange.li/feminino"
+};
 
-  const createImageElement = (src) => {
-    const image = document.createElement('img');
-    image.src = src;
-    return image;
-  };
+const bodyStyles = document.body.style;
+bodyStyles.display = 'flex';
+bodyStyles.gap = '.5em';
+bodyStyles.flexWrap = 'wrap';
 
-  const createParagraphElement = (text) => {
-    return createHTMLElement('p', text);
-  };
+const jogadoresContainer = document.getElementById('jogadores-container');
+const carregandoElement = document.getElementById('carregando');
 
-  const createBackLink = () => {
-    const backLink = document.createElement('a');
-    backLink.className = 'back-link';
-    backLink.innerHTML = 'Voltar';
-    backLink.href = './index.html';
-    backLink.style.color = '#ffffff';
-    backLink.style.fontSize = '23pt';
-    backLink.style.textDecoration = 'none';
-    return backLink;
-  };
+const preencheJogador = (atleta) => {
+    const container = document.createElement('article');
+    const titulo = document.createElement('h3');
+    const imagem = document.createElement('img');
 
-  const playerDescriptionElement = document.getElementById('player_description');
-  playerDescriptionElement.style.textAlign = 'center';
-  playerDescriptionElement.style.textTransform = 'uppercase';
-  playerDescriptionElement.style.fontWeight = 'bolder';
-  playerDescriptionElement.style.fontFamily = 'Arial, Helvetica, sans-serif';
+    const { id, altura, nome_completo, nascimento, tipo, nome, imagem: imgSrc } = atleta;
 
-  playerDescriptionElement.appendChild(createImageElement(localStorage.getItem('image')));
-  playerDescriptionElement.appendChild(createHTMLElement('p', localStorage.getItem('name')));
-  playerDescriptionElement.appendChild(createHTMLElement('p', localStorage.getItem('position')));
+    container.dataset.id = id;
+    container.dataset.altura = altura;
+    container.dataset.nome_completo = nome_completo;
+    container.dataset.nascimento = nascimento;
+    container.dataset.tipo = tipo;
 
-  const detailsDescriptionElement = document.getElementById('details_description');
-  detailsDescriptionElement.appendChild(createHTMLElement('p', localStorage.getItem('description')));
-  detailsDescriptionElement.appendChild(createHTMLElement('p', 'Nome completo: ' + localStorage.getItem('full_name')));
-  detailsDescriptionElement.appendChild(createHTMLElement('p', 'Nascimento: ' + localStorage.getItem('birthdate')));
-  detailsDescriptionElement.appendChild(createHTMLElement('p', 'Altura: ' + localStorage.getItem('height')));
+    titulo.innerText = nome;
+    imagem.src = imgSrc;
+    imagem.alt = `Imagem de ${nome}`;
 
-  const allElement = document.getElementById('player_description_container');
-  allElement.appendChild(playerDescriptionElement);
-  allElement.appendChild(detailsDescriptionElement);
+    container.addEventListener('click', handleClick);
 
-  document.body.appendChild(allElement);
-  document.body.appendChild(createBackLink());
-});
+    container.appendChild(titulo);
+    container.appendChild(imagem);
+
+    jogadoresContainer.appendChild(container);
+};
+
+const handleClick = (e) => {
+    const artigo = e.target.closest('article');
+    document.cookie = `id=${artigo.dataset.id}`;
+    document.cookie = `nome_completo=${artigo.dataset.nome_completo}`;
+    document.cookie = `nascimento=${artigo.dataset.nascimento}`;
+    document.cookie = `altura=${artigo.dataset.altura}`;
+
+    const dados = {
+        id: artigo.dataset.id,
+        nome_completo: artigo.dataset.nome_completo,
+        nascimento: artigo.dataset.nascimento,
+        altura: artigo.dataset.altura,
+        tipo: artigo.dataset.tipo
+    };
+
+    localStorage.setItem('dados-original', JSON.stringify(artigo.dataset));
+    localStorage.setItem('dados', JSON.stringify(dados));
+
+    console.log(achaCookie('nome_completo'));
+    console.log(localStorage.getItem('id'));
+    console.log(JSON.parse(localStorage.getItem('dados')).altura);
+
+    window.location = `jogadores.html?id=${artigo.dataset.id}&nome_completo=${artigo.dataset.nome_completo}`;
+};
+
+const achaCookie = (chave) => {
+    const listaDeCookies = document.cookie.split("; ");
+    const procurado = listaDeCookies.find((e) => e.startsWith(chave));
+    return procurado.split("=")[1];
+};
+
+const obterDados = async (caminho) => {
+    const resposta = await fetch(caminho);
+    const dados = await resposta.json();
+    return dados;
+};
+
+const limparJogadores = () => {
+    jogadoresContainer.innerHTML = '';
+};
+
+const exibirCarregando = () => {
+    carregandoElement.style.display = 'block';
+};
+
+const ocultarCarregando = () => {
+    carregandoElement.style.display = 'none';
+};
+
+const filtrarJogadores = async (tipo) => {
+    limparJogadores();
+    exibirCarregando();
+
+    const apiUrl = endpoints[tipo.toLowerCase()] || endpoints.all;
+
+    try {
+        const entrada = await obterDados(apiUrl);
+
+        for (const atleta of entrada) {
+            preencheJogador(atleta);
+        }
+
+    } catch (error) {
+        console.error('Erro ao carregar jogadores:', error);
+    } finally {
+        ocultarCarregando();
+    }
+};
